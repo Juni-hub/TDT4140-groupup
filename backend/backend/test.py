383b3f052add131e64533e15868a407fbea1df5c
Juni <junitrigger@gmail.com>
2022-02-15 18:09:40 +0100

    Start on tests

--- backend/backend/test.py
+++ backend/backend/test.py
@@ -1,3 +1,4 @@
+from json import JSONDecoder
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -25,3 +26,43 @@ class RegisterTest(APITestCase):
         self.assertEqual((user.username, user.email, user.first_name, user.last_name), 
             ("trondk","trondk@gmail.com","Trond","Kristiansen")
         )
+        
+class GetTest(APITestCase):
+    def test_fetch_user(self):
+        """
+        Ensure we can fetch user information
+        """
+            #Register user
+        registerUrl = reverse('register')
+        data = {
+            "username":"trondk",
+            "password":"password",
+            "email":"trondk@gmail.com",
+            "profile":{"age":20},
+            "first_name":"Trond",
+            "last_name":"Kristiansen"
+        }
+        registerResponse = self.client.post(registerUrl, data, format='json').json()
+        
+            #Fetch user
+        userUrl = reverse('user')
+        print(registerResponse)
+        token= registerResponse['token']
+        header = {'Authorization':'Token ' + token}
+        print(header)
+        response = self.client.get(userUrl, headers=header).json()
+        
+        responseData = {
+            "username":"trondk",
+            "email":"trondk@gmail.com",
+            "id":1,
+            "first_name":"Trond",
+            "last_name":"Kristiansen",
+            "age": 20
+        }
+        
+        print(response)
+        
+        for key,value in responseData.items():
+            self.assertEqual(value,response[key])
+        
