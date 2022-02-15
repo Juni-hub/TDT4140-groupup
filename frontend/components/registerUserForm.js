4c9dc6bf6b58548a67f27ee9a7b70a0028341f43
ThomasNitsche <thomanit@stud.ntnu.no>
2022-02-15 16:39:21 +0100

    make new profile form

--- frontend/components/registerUserForm.js
+++ frontend/components/registerUserForm.js
@@ -1,38 +1,48 @@
 import React from "react";
 import styles from "../styles/Home.module.css"
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, CardBody, CardHeader, Input, Label, Row, Col, Card, Form ,InputGroup, InputGroupText} from "reactstrap";
 
 
 const  RegisterUserForm = () => {
     return(
-        <div className={styles.registerContainer}>
-            <h2>REGISTER NEW USER</h2>
-
-            <form action="submit" className={styles.registerForm}>
-                <label className={styles.registerField}> First name: 
-                    <input className={styles.registerInput} type="text" name="firstName"/>
-                </label>
-
-                <label className={styles.registerField}> Last name: 
-                    <input className={styles.registerInput} type="text" name="lastName" />
-                </label>
-
-                <label className={styles.registerField}> User name:  
-                    <input className={styles.registerInput} type="text" name="userName" />
-                </label>
-
-                <label className={styles.registerField}> Age:  
-                    <input className={styles.registerInput} type="number" name="userName" />
-                </label>
-
-                <label className={styles.registerField}> Password:  
-                    <input className={styles.registerInput} type="password" name="userName" />
-                </label>
-
-                <button className={styles.registerButton} type="submit"> REGISTER</button>
-            </form>
-        </div>
+        <>
+           <Row style={{ height: "15vh" }}></Row>
+           <Row>
+               <Col xs ="4"></Col>
+               <Col xs="4">
+                   <Card>
+                       <CardHeader style={{backgroundColor:"rgba(132, 222, 89, 0.8)"}}> Register new user</CardHeader>
+                       <CardBody>
+                            <Form onSubmit="">
+                                <InputGroup>
+                                    <InputGroupText style={{width:"100px"}}>First name</InputGroupText>
+                                    <Input type="text" placeholder="First Name" name="firstName"></Input>
+                                </InputGroup>
+                                <br />
+                                <InputGroup>
+                                    <InputGroupText style={{width:"100px"}}>Last name</InputGroupText>
+                                    <Input type="text" placeholder="Last Name" name="lastName"></Input>
+                                </InputGroup>
+                                <br />
+                                <InputGroup>
+                                    <InputGroupText style={{width:"100px"}}>Age</InputGroupText>
+                                    <Input type="number" placeholder="Age" name="age"></Input>
+                                </InputGroup>
+                                <br />
+                                <InputGroup>
+                                    <InputGroupText style={{width:"100px"}}>Password</InputGroupText>
+                                    <Input type="password" placeholder="password" name="password"></Input>
+                                </InputGroup>
+                                <br />
+                                <Button type="submit" style={{width:"300px", backgroundColor:"rgba(132, 222, 89, 0.8)", color:"black" }}>Register</Button>
+                            </Form>
+                       </CardBody>
+                   </Card>
+               </Col>
+           </Row>
+        </>
     )
-}
+};
 
 export default RegisterUserForm;
