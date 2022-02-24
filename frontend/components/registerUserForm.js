f8dd76e3ff663f35c1101ec3529730beaf7388a2
Juni <junitrigger@gmail.com>
2022-02-24 21:50:35 +0100

    17: Start on age check frontend

--- frontend/components/registerUserForm.js
+++ frontend/components/registerUserForm.js
@@ -1,37 +1,99 @@
+
 import React from "react";
 import styles from "../styles/Home.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, Card, CardBody, CardHeader, Col, Form, Input, InputGroup, InputGroupText, Label, Row } from "reactstrap";
+import React, {useState} from "react";
 
-
+import { useRouter } from 'next/router'
 
 const  RegisterUserForm = () => {
+    const[isValid,setIsValid] = useState(true); 
+    const [ageIsValid, setAgeIsValid] = useState(true);
+    const router = useRouter();
+    const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify(body),
+    }
+    const response = await fetch(`http://localhost:8000/register/`, requestOptions)
+    .then(
+        async (response) =>{
+            if(response.status!=201){
+                setIsValid(false);
+                return
+            }
+            const data = await response.json()
+            console.log(data);
+            localStorage.setItem("Token", data.token)
+            router.push("/loginPage");
+        }
+    ).catch(
+        (error) =>{
+            console.log(error)
+        }
+    )
+
+const handleAgeChange = (e) => {
+    setAgeIsValid(true);
+    const age = e.target.value;
+    console.log(age);
+    const ageRegEx = new RegExp("/^(1[89]|[2-9][0-9])$");
+    if (!ageRegEx.test((age))) {
+        setAgeIsValid(false);
+    }
+    console.log(ageIsValid);
+}
     return(
-        <div className={styles.registerContainer}>
-            <h2>REGISTER NEW USER</h2>
-
-            <form action="submit" className={styles.registerForm}>
-                <label className={styles.registerField}> First name: 
-                    <input className={styles.registerInput} type="text" name="firstName"/>
-                </label>
-
-                <label className={styles.registerField}> Last name: 
-                    <input className={styles.registerInput} type="text" name="lastName" />
-                </label>
-
-                <label className={styles.registerField}> User name:  
-                    <input className={styles.registerInput} type="text" name="userName" />
-                </label>
-
-                <label className={styles.registerField}> Age:  
-                    <input className={styles.registerInput} type="number" name="userName" />
-                </label>
-
-                <label className={styles.registerField}> Password:  
-                    <input className={styles.registerInput} type="password" name="userName" />
-                </label>
-
-                <button className={styles.registerButton} type="submit"> REGISTER</button>
-            </form>
-        </div>
+        <>
+           <Row style={{ height: "15vh" }}></Row>
+           <Row>
+               <Col xs ="4"></Col>
+               <Col xs="4">
+                   <Card>
+                       <CardHeader style={{backgroundColor:"rgba(132, 222, 89, 0.8)"}}> Register new user</CardHeader>
+                       <CardBody>
+                            <Form onSubmit={handleSubmit}>
+                                <InputGroup>
+                                    <InputGroupText style={{width:"100px"}}>Brukernavn</InputGroupText>
+                                    <Input type="text" placeholder="Brukernavn" name="username"></Input>
+                                </InputGroup>
+                                <br/>
+                                <InputGroup>
+                                    <InputGroupText style={{width:"100px"}}>Fornavn</InputGroupText>
+                                    <Input type="text" placeholder="Fornavn" name="firstName"></Input>
+                                </InputGroup>
+                                <br />
+                                <InputGroup>
+                                    <InputGroupText style={{width:"100px"}}>Etternavn</InputGroupText>
+                                    <Input type="text" placeholder="Etternavn" name="lastName"></Input>
+                                </InputGroup>
+                                <br />
+                                <InputGroup>
+                                    <InputGroupText style={{width:"100px"}}>Alder</InputGroupText>
+                                    <Input type="text" placeholder="Alder" name="age" onBlur={(e)=>handleAgeChange(e)} ></Input>
+                                </InputGroup>
+                                {!ageIsValid && <div style={{ color: "red" }}>Age must be 18 or over</div>}
+                                {ageIsValid && <div style={{ color: "red" }}>yes</div>}
+                                <br />
+                                <InputGroup>
+                                    <InputGroupText style={{width:"100px"}}>E-mail</InputGroupText>
+                                    <Input type="email" placeholder="E-mail" name="email"></Input>
+                                </InputGroup>
+                                <br />
+                                <InputGroup>
+                                    <InputGroupText style={{width:"100px"}}>Passord</InputGroupText>
+                                    <Input  type="password" placeholder="Passord" name="password"></Input>
+                                </InputGroup>
+                                <br />
+                                <Button type="submit" style={{backgroundColor:"rgba(132, 222, 89, 0.8)", color:"black" }}>Register</Button>
+                                {!isValid && <p>Dataen du sendte inn var ugyldig</p>}
+                            </Form>
+                       </CardBody>
+                   </Card>
+               </Col>
+           </Row>
+        </>
     )
 }
 
