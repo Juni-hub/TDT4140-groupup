f6044eb79172ea3eaa27a240f37e43f839c6e8d1
ThomasNitsche <thomanit@stud.ntnu.no>
2022-03-03 09:53:50 +0100

    25: Made group page use next dynamic routing
    Co-authored-by: Halvor Linder <@halvorlh>

---
+++ frontend/pages/groupPage/[id].js
@@ -0,0 +1,14 @@
+import react from "react";
+import GroupPageComponent from "../../components/groupPageComponent";
+
+
+
+const GroupPage = () =>{
+    return(
+        <div>
+            <GroupPageComponent />
+        </div>
+    )
+}
+
+export default GroupPage;
