79397f0eb1ba40c6067ae29c73477270eec18b03
Juni <junitrigger@gmail.com>
2022-03-22 13:13:52 +0100

    42: Add matched group page

---
+++ frontend/pages/groupPage/[id]/matchedGroups/[otherId].js
@@ -0,0 +1,13 @@
+import GroupPageComponent from "../../components/matchedGroupPageComponent";
+import react from "react";
+
+const matchedGroupPage = () =>{
+    return(
+        <div className="">
+            <NavigationBar />
+            <MatchedGroupPageComponent />
+        </div>
+    )
+}
+
+export default matchedGroupPage;
